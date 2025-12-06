c3f18b09a4f4a36488eab6fec0c4910abf5f1109
Aviv <avivbe95@gmail.com>
2025-12-06 09:24:01 +0200

    adding main

---
+++ main.py
@@ -0,0 +1,10 @@
+import os
+
+print("\n--- Running: data_collectoin.py ---")
+os.system("python data_collectoin.py")
+
+print("\n--- Running: sensing.py ---")
+os.system("python sensing.py")
+
+print("\n--- Running: dataset_creation.py ---")
+os.system("python dataset_creation.py")
